5ae419fd74edfd4ad9afcb9db07c9258e6e59ec4
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-02-19 18:45:55 +0600

    c21

---
+++ JS Core Concept/concept21.js
@@ -0,0 +1,19 @@
+//!!(Double not) 
+
+console.log(!!"Hello");  //true
+
+console.log(!!42);   //true
+
+console.log(!!{});    //true
+
+console.log(!![]);   //true
+
+console.log(!!"");   //false
+
+console.log(!!0);    //false
+
+console.log(!!null);  //false
+
+console.log(!!undefined);   //false
+
+console.log(!!NaN);   //false
