15b5f2ada01dd1f55931fde32b608089099bca26
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-02-21 18:47:39 +0600

    c28

---
+++ JS Core Concept/concept28.js
@@ -0,0 +1,25 @@
+//===
+
+console.log(2 === "2");
+
+console.log(1 === true);
+
+console.log(null === undefined);
+
+console.log("" === 0);
+
+console.log(false === "0");
+
+
+//==
+
+
+console.log(2 == "2");
+
+console.log(1 == true);
+
+console.log(null == undefined);
+
+console.log("" == 0);
+
+console.log(false == "0");
