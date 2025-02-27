621e83e8cd5f7c5c6c5a9e34cfa6b4080e19acc2
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-02-27 18:50:20 +0600

    c39

---
+++ JS Core Concept/concept39.js
@@ -0,0 +1,11 @@
+ const numbers = [2,4,6,8];
+
+ const doubleNumbers = numbers.map(num => num * 2);
+ console.log(doubleNumbers);
+
+ const number = [1,3,5,7];
+ const tripleNumbers = number.map(function(n){
+    return n * 3;
+ });
+
+ console.log(tripleNumbers);
