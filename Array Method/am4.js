47b7e4d0301b579c77c3b341258ca5c63f61cd31
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-02 21:56:15 +0600

    m4

---
+++ Array Method/am4.js
@@ -0,0 +1,5 @@
+const friend = ["SUdipto","SAyem","FAruk"];
+const lengths = friend.map(lng => lng.length);
+console.log(lengths);
+const firstLetter = friend.map(frnd => frnd[0]);
+console.log(firstLetter);
