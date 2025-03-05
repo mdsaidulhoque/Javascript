4df251450e34698bf3285de93e9058bf76395ad7
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-05 14:11:45 +0600

    variable

---
+++ variables.js
@@ -0,0 +1,93 @@
+//Variable declaration
+
+
+const weight = 53;
+const height = 5.7;
+const birthdate = "7/6/2003";
+const siblings = "3 brothers and 1 sister";
+const students = "more then 200 I guess";
+const marks = 28/40;
+const rooms = 3;
+const studytime = "1 hours+";
+const totalNumber = 50;
+
+
+//log console number type
+
+
+console.log(951);
+
+const age = 21;
+console.log(age);
+
+const bookToRead = 16;
+console.log(bookToRead);
+
+let wakeUpTime = 7;
+wakeUpTime = 8;
+console.log(wakeUpTime);
+
+const wasteTime = 3;
+console.log(wasteTime);
+
+const borrow = 500;
+ 
+const goal = 3;
+console.log(goal);
+
+const pillow = 2;
+console.log(pillow);
+
+
+//log console string type
+
+
+const name = "MD SAIDUL HOQUE";
+console.log(name);
+
+const address = "Chittagong";
+console.log(address);
+
+const university = "National University";
+console.log(university);
+
+const fan = "Hinata";
+console.log(fan);
+
+const movieOfTheYear = "Lucky Bhaskar";
+console.log(movieOfTheYear);
+
+const hobby = "Sleeping";
+console.log(hobby);
+
+const color = "Blue";
+console.log(color);
+
+
+////log console boolean type
+
+
+const modeHappy = true;
+console.log(modeHappy);
+
+const crushReplied = false;
+console.log(crushReplied);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
