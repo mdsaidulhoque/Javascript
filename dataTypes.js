24e5d290ab9c55fb085e3ba602c9bccbe0db2710
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-05 23:35:16 +0600

    code3

---
+++ dataTypes.js
@@ -0,0 +1,127 @@
+//Check data types
+
+const price = 555;
+console.log(typeof price);
+
+const learning = true;
+console.log(typeof learning);
+
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+const temparature = 102.5;
+console.log(typeof temparature);
+
+const isHungry = true;
+console.log(typeof isHungry);
+
+const isRich = true;
+console.log(typeof isRich);
+
+
+
+//let
+
+let age = 18;
+console.log(age);
+age = 21;
+console.log(age);
+
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+
+let studyTime = 4;
+studyTime = 2;
+console.log(studyTime);
+
+
+let friendCount = 3;
+friendleft = 2;
+friendCount = friendCount - friendleft;
+console.log(friendCount);
+
+
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+
+let chairCount = 40;
+chairCount = chairCount - 2;
+chairCount = chairCount + 6;
+console.log(chairCount);
+
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
+
+
+
+
+
+//Comment
+
+//It's my math exam score 
+let score = 54;
+
+
+/*
+The distance from the centre of a circle to any point on the boundary is called the radius. 
+The radius is half of the diameter; 2r=d 2 r = d . 
+The line segment that joins two points on the circle is a chord.
+*/
+let radius;
+
+
+
+
+//Parse(NaN)
+
+let pnum = '20';
+pnum = parseInt('20');
+let qnum = 10;
+totalpqnum = pnum + qnum;
+console.log(totalpqnum);
+
+
+let pai = '3.14159';
+pai = parseInt('3.14159');
+console.log(pai); 
+
+
+let pstr = 'premikBoy';
+pstr = parseInt('premikBoy');
+console.log(pstr);
+
+
+const num7 = 3.456;
+const num8 = 2.789;
+const total78num = num7 + num8;
+const Total78 = parseFloat(total78num.toFixed(2));
+console.log(Total78)
+
+
+let num11 = '56.78';
+num11 = parseFloat('56.78');
+let num12 = '12.34';
+num12 = parseFloat('12.34');
+const total1112 = num11 + num12;
+console.log(total1112);
+
+
+let num13 = '10.5678';
+num13 = parseFloat('10.5678');
+let num14 = '5.4321';
+num14 = parseFloat('5.4321');
+let total1314 = num13 + num14;
+let total134 = parseFloat(total1314.toFixed(2))
+console.log(total134)
+
+
+let num16 = '42.45689754';
+let num17 = parseFloat(num16);
+console.log(num17.toFixed(3));
