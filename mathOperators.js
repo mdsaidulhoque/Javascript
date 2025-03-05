777df331344cd158b8696496472324336ab555e2
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-05 16:47:30 +0600

    code2

---
+++ mathOperators.js
@@ -0,0 +1,79 @@
+//Math operators
+
+const income = 100;
+const spending = 40;
+const remaining = income - spending;
+console.log(remaining);
+
+
+ const pencil = 10;
+ const perUse = 2;
+ const totalUse = pencil / perUse;
+ console.log(totalUse);
+
+
+
+ const money = 60;
+ const perSpending = 15;
+ const totalHowManyTimes = money / perSpending;
+ const remainingTotal = money % perSpending;
+ console.log(totalHowManyTimes);
+ console.log(remainingTotal);
+
+
+
+ const str1 = "Hello";
+ const str2 = "World";
+ const str = str1 + str2;
+ console.log(str);
+
+
+
+ const riceKg = 153;
+ const dalkg = 261;
+ const totalKg = riceKg + dalkg;
+ console.log(totalKg);
+
+
+
+
+ const haveMoney = 500;
+ const haveSpended = 75;
+ const howManyTimes = haveMoney / haveSpended;
+ const haveRemaining = haveMoney % haveSpended;
+ console.log(howManyTimes);
+ console.log(haveRemaining);
+
+
+
+const num1 = 8;
+const num2 = 3;
+const num3 = 8/3;
+const num4 = 8 % 3 ;
+console.log(num3);
+console.log(num4);
+
+
+
+const anum = 50;
+const bnum = 9;
+const cnum = anum % bnum;
+console.log(cnum);
+
+
+
+
+
+const stn1 = "Bangla";
+const stn2 = "desh";
+const stn = stn1 + stn2;
+console.log(stn);
+
+
+
+const needOfRiceDailyKg = 4;
+const totalRiceKg = 480;
+const numOfDays = totalRiceKg / needOfRiceDailyKg;
+console.log(numOfDays);
+const numOfMonths = numOfDays / 30;
+console.log(numOfMonths);
