e76a08873e81c5e9be91b31e8a311dbdbec67e47
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-08 01:41:38 +0600

    code5

---
+++ comparison.js
@@ -0,0 +1,41 @@
+const myAge = 15;
+const myBrotherAge = 10;
+console.log(myAge >= myBrotherAge);
+
+
+const passMark = 33;
+const myMarks = 45;
+console.log(passMark <= myMarks);
+
+
+const applePrice = 230
+const orangePrice = 260;
+console.log(applePrice < orangePrice);
+
+
+const booksReadByMe = 10;
+const booksReadByMyFriend = 10;
+console.log(booksReadByMe == booksReadByMyFriend);
+
+
+const myBiscuitTotal = 15;
+const friendBiscuitTotal = 20;
+console.log(myBiscuitTotal < friendBiscuitTotal);
+
+
+
+const realMadridGoal = 2;
+const bracelonaGoal = 3;
+console.log(realMadridGoal < bracelonaGoal);
+
+
+//false
+const myName = "Saidul";
+const friendName = "Faruk"
+console.log(myName == friendName);
+
+
+
+const studyByMe = true;
+const studyByMyFriend = false;
+console.log(studyByMe == studyByMyFriend);
