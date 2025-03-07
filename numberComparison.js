f3db926fa8c16b3ce6cf0ce163fb6e39ae4fa7bb
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-08 01:15:37 +0600

    code4

---
+++ numberComparison.js
@@ -0,0 +1,29 @@
+  const sandwichEatenByMe = 7;
+  const sandwichEatenByMyBrother = 5;
+  console.log(sandwichEatenByMe > sandwichEatenByMyBrother);
+
+
+  const theNumberOfStudentInMyClass = 45;
+  const theNumberOfStudentInNextClass = 50;
+  console.log(theNumberOfStudentInMyClass < theNumberOfStudentInNextClass);
+
+
+  const myMarksOnExam = 75;
+  const myFriendMarksOnExam = 75;
+  console.log(myMarksOnExam == myFriendMarksOnExam);
+
+
+  const targetGoal = 100;
+  const presentGoal = 95;
+  console.log(targetGoal <= presentGoal);
+
+
+  const num1 = 10;
+  const num2 = 7;
+  console.log(num1 != num2);
+
+
+  const myTotalMarks = 45;
+  const myFriendTotalMarks = 97;
+  console.log(myTotalMarks > myFriendTotalMarks);
+  
