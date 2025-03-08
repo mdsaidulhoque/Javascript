b2810729bb05bd97de444194015f1a8985bf41ca
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-08 16:48:45 +0600

    code6

---
+++ advanceComparison.js
@@ -0,0 +1,47 @@
+const moneyIHave = 50;
+const moneyFriendHave = "50";
+console.log(moneyIHave == moneyFriendHave);
+console.log(moneyIHave === moneyFriendHave);
+
+
+
+const myNotebook = "JavaScript";
+const myFriendNoteBook = 'JavaScript';
+console.log(myNotebook == myFriendNoteBook);
+console.log(myNotebook === myFriendNoteBook);
+
+
+//if (==) then comparison of the value of both variables
+//if (===) then comparison of the value and data types of both variables
+const num1 = 25;
+const num2 = "25";
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+
+
+ const fruit1 = "apple";
+ const fruit2 = "apple";
+ console.log(fruit1 == fruit2);
+ console.log(fruit1 === fruit2);
+
+
+
+ const str1 = "test";
+ const str2 = "TEXT";
+ console.log(str1 == str2);
+ console.log(str1 === str2);
+
+
+
+ const variableX = 15;
+ const variableY = "20";
+ console.log(variableX != variableY);
+ console.log(variableX !== variableY);
+
+
+
+ const variableA = "hello";
+ const variableB = "Hello";
+ console.log(variableA == variableB);
+ console.log(variableA === variableB);
