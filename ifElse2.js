2edcd0257c73ae424279864e8d46444bbf776904
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-08 19:14:56 +0600

    code8

---
+++ ifElse2.js
@@ -0,0 +1,100 @@
+const age = 19;
+const height = 60;
+if(age > 18 && height >60) {
+    console.log("He will help you to push the car");
+}
+else{
+    console.log("He wil just stay in the car");
+}
+
+
+
+const mathScore = 90;
+const englishScore = 78;
+if (mathScore > 80 || englishScore > 85) {
+    console.log("You are eligible for admission");
+}
+else{
+    console.log("Go away");
+}
+
+
+
+
+const GPA = 5;
+const monthlyIncome = 22000;
+if (GPA == 5 && monthlyIncome < 15000){
+    console.log("You are eligible for scholarship");
+}
+else{
+    console.log("You will have to pay tuition fees");
+}
+
+
+
+
+const Age = 28;
+const yearsOfExperience = 2;
+if(Age < 30 && yearsOfExperience > 2){
+    console.log("You will be able to apply for this job");
+}
+else{
+    console.log("You can't apply for this job")
+}
+
+
+
+const eggsInTheFridge= 16;
+const chickenInTheFridge = 0;
+if (eggsInTheFridge > 12 || chickenInTheFridge == 0){
+    console.log("I will cook speacial curry of egg");
+}
+else{
+    console.log("I will just eat banana and bread");
+}
+
+
+
+const Tempearture = 94;
+const Cough = true;
+if (Tempearture > 100 || Cough == true){
+    console.log("You have a fever,you should go to hostpital");
+}
+else{
+    console.log("You just need to get enough sleep");
+}
+
+
+
+
+const attendencePercentage = 82;
+const submitAssignment = true;
+if (attendencePercentage > 80 && submitAssignment == true){
+    console.log("You will be able to participate in exam");
+}
+else {
+    console.log("Auto fail");
+}
+
+
+
+const electricityIsGone  = true;
+const mobileCharged = false;
+if (electricityIsGone == true && mobileCharged == false){
+    console.log("I will study");
+}
+else{
+    console.log("I will just play video games");
+}
+
+
+
+
+const shirtPrice = 1000;
+const coupon = true;
+if(shirtPrice > 1000 && coupon == true){
+    console.log("You will get 20% discount");
+}
+else{
+    console.log("no discount");
+}
