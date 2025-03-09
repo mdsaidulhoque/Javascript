0b085d4628cf083d5a6ecefb480285a8a33cc251
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-10 01:35:25 +0600

    bcommit2

---
+++ Array JS/array2.js
@@ -0,0 +1,25 @@
+//Index
+
+const friendsName =['Sayed','Sayem','Abdur Rahim','Motasim','Faruk','Sudipto'];
+const indexNum = friendsName[3];
+console.log(indexNum);
+
+
+const Books = ['Linear Algebra','Calculas','Geometry','Fundamental of Mathmatics','Statistics','Physics'];
+const numOfBook = Books[5];
+console.log(numOfBook);
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+numbers[7] = 30;
+console.log(numbers);
+
+
+const gamesName = ['PUBG','Candy Crush','Temple Run','Ludo',];
+gamesName[2] = "Subway Surfer";
+console.log(gamesName);
+
+
+const inTable = ['Pen','Pencil','Shapner','Watch','Mobile'];
+const seventh = inTable[7];
+console.log(seventh);
