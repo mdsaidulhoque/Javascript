245707300fbf851fccd946c564b81f786695e627
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-09 23:28:31 +0600

    commit4

---
+++ Nested If Else/nestedIfElse4.js
@@ -0,0 +1,13 @@
+const checkFoodInTheFridge = false;
+const checkDelievery = true;
+if(checkFoodInTheFridge == true){
+    console.log("Warm the food");
+}
+else{
+    if(checkDelievery){
+         console.log("Order a pizza from Dominos Pizza");
+    }
+    else{
+        console.log("I am shaman today");
+    }
+}
