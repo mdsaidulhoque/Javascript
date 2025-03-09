1274e7a8d2cd032230db3381322f602a90aca26c
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-09 23:37:15 +0600

    commit5

---
+++ Nested If Else/nestedIfElse5.js
@@ -0,0 +1,10 @@
+const numOfGuest = 120;
+const bringGiftsByTheGuest = true;
+if(numOfGuest > 100){
+    if(bringGiftsByTheGuest){
+        console.log("Lets party all night");
+    }
+    else{
+        console.log("I will party with myself");
+    }
+}
