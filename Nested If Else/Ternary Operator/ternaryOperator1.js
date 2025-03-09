3229b0ed460d97bf4b31557ee948e160ede84a89
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-10 00:12:14 +0600

    acommit1

---
+++ Nested If Else/Ternary Operator/ternaryOperator1.js
@@ -0,0 +1,12 @@
+const age1 = 21;
+if(age1 >15){
+    console.log("Teenager");
+}
+else{
+    console.log("Child");
+}
+
+
+
+const age = 21;
+age > 15 ? console.log("Teenager") : console.log("child");
