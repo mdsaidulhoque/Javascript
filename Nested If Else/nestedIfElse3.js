a04427b592129ea969df1a1ec469402cd1612710
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-09 19:36:05 +0600

    commit3

---
+++ Nested If Else/nestedIfElse3.js
@@ -0,0 +1,14 @@
+const isActive = true;
+const havePremium = false;
+if(isActive){
+    if(havePremium){
+        console.log("Enjoy the the premium version");
+    }
+    else{
+        console.log("Enjoy the free version");
+    }
+    
+}
+else{
+    console.log("Active your account first");
+}
