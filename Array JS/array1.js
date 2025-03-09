84e957fe1e49f79fe806595401c84ebc3552a2f2
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-10 01:12:01 +0600

    bcommit1

---
+++ Array JS/array1.js
@@ -0,0 +1,17 @@
+//Length
+
+const numbers = [71,72,73,74,75,76,77,78,79];
+const size = numbers.length;
+console.log(size);
+
+
+const Fruits = ['Apple','Banana','Orange','Mango','Pineapple'];
+console.log(Fruits.length);
+
+
+const Vegetable = ['Beetroot','Cabbage','Carrot','Cucumber','Pumpkin','Tomato'];
+console.log(Vegetable.length);
+
+
+const City = ['Dhaka','New York','Tokyo','Berlin','Busan','Karachi','Mumbai'];
+console.log(City.length);
