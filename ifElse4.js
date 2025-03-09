73743a70c0748285d80965163b634ff97dd52b71
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-09 18:48:23 +0600

    code12

---
+++ ifElse4.js
@@ -0,0 +1,49 @@
+const score = 52;
+if (score < 50) {
+    console.log("Fail");
+}
+else if (score < 80) {
+    console.log("Pass");
+}
+else{
+    console.log("A+");
+}
+
+
+
+const numOfPages = 447;
+if (numOfPages < 100) {
+    console.log("Small book");
+}
+else if (numOfPages < 500) {
+    console.log("Mid-size book");
+}
+else{
+    console.log("heart-attack size book");
+}
+
+
+
+const bTemperature = 21;
+if (bTemperature < 0) {
+    console.log("Ice");
+}
+else if (bTemperature < 20){
+    console.log("Cool Cool");
+}
+else {
+    console.log("Hot Hot");
+}
+
+
+
+const gamingLevel = 72;
+if (gamingLevel < 10){
+    console.log("novie");
+}
+else if(gamingLevel < 50){
+    console.log("Expert");
+}
+else{
+    console.log("Pro Gamer");
+}
