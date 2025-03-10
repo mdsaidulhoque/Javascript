1ef006c7307bdfaf5baef57089ab435909fc376c
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-10 17:57:50 +0600

    ccommit1

---
+++ Array JS/includesArray/includes1.js
@@ -0,0 +1,7 @@
+const Fruits = ['Apple','Banana','Orange','Mango','Pineapple'];
+if(Fruits.includes('Mango')){
+    console.log("There are Mangoes");
+}
+else{
+    console.log("There is no Mangoes,climp up the tree");
+}
