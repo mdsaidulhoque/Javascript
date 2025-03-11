6572bf130b35d280ebab588521370e160558624c
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-12 00:27:58 +0600

    dcommit6

---
+++ Loop/For of loop/forOfLoop6.js
@@ -0,0 +1,4 @@
+const Team11 = ['Tamim','Liton','Somyo','Shakib','Mushfiq','Mahmodullah','Afif','Rishad','Shafique','Mustapiz','Taskin'];
+for (const playerName of Team11){
+    console.log(playerName);
+}
