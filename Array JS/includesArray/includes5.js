4b53b33b9545fc109b7261f9ed577db3c97da47f
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-11 11:53:28 +0600

    ccommit5

---
+++ Array JS/includesArray/includes5.js
@@ -0,0 +1,8 @@
+const myArray = ['Lake','Cloud','Rain','Rainfall'];
+const checkArray = myArray.includes('Rain');
+if(checkArray){
+    console.log("I need umberalla");
+}
+else{
+    console.log("No rain no pain");
+}
