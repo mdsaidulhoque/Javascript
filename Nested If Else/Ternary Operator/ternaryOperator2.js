29dba8c5245027c799f27b2f154ca8ff33210489
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-14 01:15:26 +0600

    acommit2

---
+++ Nested If Else/Ternary Operator/ternaryOperator2.js
@@ -0,0 +1,12 @@
+let price = 500;
+let isLeader = false;
+if (isLeader === true){
+    price = 0;
+    console.log(price);
+}
+else{
+    price = price + 100;
+    console.log(price);
+}
+
+price = isLeader ===true?  console.log(50) :  console.log(price + 50);
