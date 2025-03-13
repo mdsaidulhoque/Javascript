e57450056f0d51329fbef5c052e279b2b66c52f0
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-14 01:22:39 +0600

    s2

---
+++ Object/Structure/object2.js
@@ -0,0 +1,11 @@
+const subject = {
+    name : "Linear Algebra",
+    teacher : "Mannan Sir",
+    examdate : "30 April",
+    chapters: [1,3,4,7],
+    nextExam : {
+        name : "inCourse",
+        Marks : 15,
+    }
+}
+console.log(subject);
