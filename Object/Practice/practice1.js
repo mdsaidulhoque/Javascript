d4ccc11922b506d02d2aaf7a13468448addb2a47
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-17 00:59:18 +0600

    ap1

---
+++ Object/Practice/practice1.js
@@ -0,0 +1,9 @@
+const Books = {
+    Name: "Physics 2",
+    Writer: "M Halim",
+    Price : 230
+};
+const keys = Object.keys(Books);
+console.log(keys);
+const values = Object.values(Books);
+console.log(values);
