a7f56807ebbd231c0d378f3a25265204b85106d7
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-18 17:19:08 +0600

    k6

---
+++ Object/Property/keysValues6.js
@@ -0,0 +1,12 @@
+const computer = {
+    brand : "Asus",
+    price : 45000,
+    processor:"intel",
+    ssd: "512gb",
+}
+if(computer.brand === "Asus"){
+    console.log("good choice");
+}
+else{
+    console.log("You should consider it");
+}
