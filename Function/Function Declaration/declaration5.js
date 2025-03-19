ded407c8e8eac18bea5f17d49363ab795b224482
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-19 17:44:41 +0600

    f5

---
+++ Function/Function Declaration/declaration5.js
@@ -0,0 +1,8 @@
+function taskDone(){
+    console.log("Wake up from sleep");
+    console.log("Get freshed");
+    console.log("Take shower");
+    console.log("Eat lunch");
+    console.log("Scroll my phone");
+}
+taskDone();
