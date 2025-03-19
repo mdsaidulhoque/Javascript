683528afe162b682c2a0b8166034641fe54ecf42
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-19 17:34:45 +0600

    p4

---
+++ Object/Property Access/valueAccess4.js
@@ -0,0 +1,14 @@
+const college = {
+    name: 'vnc',
+    class: ['11','12'],
+    events: ['science fair','bijoy dibos','21 feb'],
+    unique: {
+        color: 'blue',
+        result: {
+            gpa : 5,
+            merit: 'top',
+        }
+    }
+};
+console.log(college.unique.result.gpa);
+console.log(college.unique["color"]);
