f934506077606856e7c9b272fa0fd5d8173b34a0
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-20 02:37:43 +0600

    p5

---
+++ Function/Function Parameter/parameter5.js
@@ -0,0 +1,7 @@
+function difference(num1,num2){
+    const diff = num1 - num2;
+    console.log(num1,"and",num2,"Difference is:",diff);
+}
+const fatherAge = 60;
+const sonAge = 21;
+difference(fatherAge,sonAge);
