e80429367aab5bb51a6d8f29d0e2f575ef62626e
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-19 17:42:22 +0600

    f3

---
+++ Function/Function Declaration/declaration3.js
@@ -0,0 +1,8 @@
+function myRoutine() {
+    console.log("Get up from bed at 11am if I don't have college");
+    console.log("get freshed");
+    console.log("Eat lunch");
+    console.log("Scroll mobile");
+    console.log("Then sleeping at 2 am");
+}
+myRoutine();
