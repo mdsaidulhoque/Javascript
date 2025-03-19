1099ac6aacccb6494eb91dd8fe0f37759e8defef
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-03-19 17:39:20 +0600

    p12

---
+++ Object/Property Access/valueAccess12.js
@@ -0,0 +1,19 @@
+const family = {
+    father : {
+       name : "Mir Kashem",
+       age : 62,
+       profession : "Business",
+    },
+    mother : {
+        name : "Hasina Akter",
+        age : 50,
+        profession : "Housewife",
+    }
+
+}
+age1 = family.father.age;
+console.log(age1);
+age2 = family.mother.age;
+console.log(age2);
+Age = age1  + age2;
+console.log(Age);
