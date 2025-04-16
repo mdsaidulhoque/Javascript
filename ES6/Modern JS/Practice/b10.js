7b798491334ba0ab90e6aae161eefe35190e3d24
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-04-16 22:34:21 +0600

    b10

---
+++ ES6/Modern JS/Practice/b10.js
@@ -0,0 +1,3 @@
+const student = {name: "Saidul Hoque",age: 21,marks:[65,78,87]};
+const avg = student.marks[0] + student.marks[1] + student.marks[2];
+console.log(`Name: ${student.name}, Age: ${student.age},Average marks ${avg / 3}`);
