ece46af68a5d6818187c2034e6d74de2c3c41058
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-04-19 20:24:21 +0600

    s3

---
+++ Destructuring JS/ds3.js
@@ -0,0 +1,9 @@
+const profile = {
+    name: "Sayed",
+    roll: 190,
+    student: true
+}
+const{name,roll: serialNum,student} = profile;
+console.log(name);
+console.log(serialNum);
+console.log(student);
