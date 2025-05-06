9d4b67b92cbc69d32174259e1fabcb1bed80ef6f
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-05-06 21:51:40 +0600

    m37

---
+++ Array Method/am37.js
@@ -0,0 +1,8 @@
+const products = [
+    {id: 1,name: "Lenovo",price: 65000},
+    {id: 2,name: "dell",price: 45000},
+    {id: 3,name: "hp",price: 40000},
+    {id: 4,name: "mac",price: 65000}
+];
+const Id = products.map(p => p.id);
+console.log(Id);
