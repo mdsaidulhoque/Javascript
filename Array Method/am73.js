ef72a50ca1954bd594f6cbd8a7bb4126105d1e7a
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-05-20 22:03:50 +0600

    m73

---
+++ Array Method/am73.js
@@ -0,0 +1,4 @@
+const movieList = ["Inception","Titanic","Joker","Avatar","Interstellar"];
+const deleted  = movieList.splice(2,2,"Batman","Superman");
+console.log(movieList);
+console.log(deleted);
