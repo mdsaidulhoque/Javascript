c943cd89a903ff09fc82233b27f9e158be603f3f
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-05-24 22:10:05 +0600

    str012

---
+++ String Method/string12.js
@@ -0,0 +1,10 @@
+//Lower-Upper
+
+const subject = "Chemistry";
+const book = "chemistry";
+if (subject === book){
+    console.log("I am reading book");    
+} else{
+    console.log("Hudai prista ultai");
+    
+}
