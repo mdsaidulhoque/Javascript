f3a3a1ac6d3c923eb2d3e122c82e485487862ee1
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-05-26 21:07:19 +0600

    str015

---
+++ String Method/string15.js
@@ -0,0 +1,9 @@
+
+const subject = "Chemistry";
+const book = "chemistry";
+if (subject.toLowerCase === book.toLowerCase){
+    console.log("I am reading book");    
+} else{
+    console.log("Hudai prista ultai");
+    
+}
