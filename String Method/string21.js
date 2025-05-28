80bf1f2da31c9efc3b2f16a9f0a2b64a82273d40
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-05-28 23:44:08 +0600

    str021

---
+++ String Method/string21.js
@@ -0,0 +1,10 @@
+
+const drink = " Water";
+const liquid = "Water ";
+
+if(drink === liquid){
+    console.log("Pani hani dami jani");
+}
+else{
+    console.log("Somodrer pani dekte sundor khaite khala");
+}
