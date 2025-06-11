8ad898f57633e215c6a06562d05d08380ddefd82
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-11 13:53:48 +0600

    v1

---
+++ Error Validation/validation1.js
@@ -0,0 +1,25 @@
+function sum(num1,num2){
+
+    const total = num1 + num2;
+
+    return total;
+}
+
+const result = sum(5,7);
+
+console.log(result);
+
+//Error is if
+
+function sum1(num1,num2){
+
+    const total = num1 + num2;
+
+    return total;
+}
+
+const result1 = sum1(5,'7');
+
+console.log(result1);
+
+
