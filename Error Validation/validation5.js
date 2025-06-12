34aa9e60f72f40f937839d38ed2a72c03c813185
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-12 13:55:19 +0600

    v5

---
+++ Error Validation/validation5.js
@@ -0,0 +1,14 @@
+function getSecond(numbers){
+
+    console.log(typeof numbers);
+
+    const second = numbers[1];
+
+    return second;
+}
+
+const arr = [1,2,3,4,5]
+
+const Second = getSecond(arr);
+
+console.log(Second);
