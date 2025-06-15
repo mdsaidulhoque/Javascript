bd4b59ba28026751bb37e5af1e9664ce6c829013
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-15 13:58:20 +0600

    s5

---
+++ JS Core Concept/scope5.js
@@ -0,0 +1,12 @@
+const isPresent = true;
+
+if (isPresent){
+
+    const message = "I have attend my class today";
+
+    console.log(message);
+}
+
+//trying to access outside of the block
+
+console.log(message);
