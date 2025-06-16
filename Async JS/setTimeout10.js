92ee231853aad9fd65590111cfa13e1c70778bbe
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-16 18:51:50 +0600

    time10

---
+++ Async JS/setTimeout10.js
@@ -0,0 +1,15 @@
+console.log("Start");
+
+setTimeout( () => {
+
+    console.log("Timeout 1");
+
+}, 3000);
+
+setTimeout( () => {
+
+    console.log("Timeout 2");
+
+}, 1000);
+
+console.log("End");
