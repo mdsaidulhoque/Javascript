fa2edd85e98e006f00df26415b2dda54064fafc2
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-16 18:55:38 +0600

    json4

---
+++ JSON Fetch Promise/json4.js
@@ -0,0 +1,15 @@
+const user = {
+
+    name: "Saim",
+
+    email: "saim51@gmail.com",
+
+    address: "amar bari",
+
+    orderHistory: ["Mobile","Tab","Laptop"],
+
+}
+
+const userJSON = JSON.stringify(user);
+
+console.log(userJSON);
