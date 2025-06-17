59b19f5644d3c55d5fce6b260f9ba74682e9e521
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-17 13:59:18 +0600

    s9

---
+++ JS Core Concept/scope9.js
@@ -0,0 +1,17 @@
+let name = "Mohammad";
+
+function first(){
+
+    let name = "Saidul";
+
+    function second(){
+
+        // let name = "Hoque";
+
+        console.log(name);
+    }
+
+    second();
+}
+
+first();
