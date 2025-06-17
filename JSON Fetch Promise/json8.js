80c1c07b8dab3fff5932c06c8c6077f44eb53631
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-17 18:56:54 +0600

    json8

---
+++ JSON Fetch Promise/json8.js
@@ -0,0 +1,20 @@
+const projectManagemntSystem = {
+
+    projectDescription: "$#*@",
+
+    teamMember: ["saim","sayed","hoque"],
+
+    deadline: "End of the month",
+
+    tasks:{
+        title: "Biriyani maker",
+
+        assignee: "Ibne rafi",
+
+        status: "pending"
+    }
+}
+
+const projectManagemntSystemJSON = JSON.stringify(projectManagemntSystem);
+
+console.log(projectManagemntSystemJSON);
