3bb748ab1793f91cd89281205ca687ba6cadf97a
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-17 18:57:20 +0600

    json10

---
+++ JSON Fetch Promise/json10.js
@@ -0,0 +1,19 @@
+const productReview = {
+
+    pName: "Teheri",
+
+    rDatails: {
+        
+        name: "Afif",
+
+        email: "afis12@gmail.com",
+
+        rating: "9/10",
+
+        rText: "This teheri taste good"
+    }
+}
+
+const productReviewJSON = JSON.stringify(productReview);
+
+console.log(productReviewJSON);
