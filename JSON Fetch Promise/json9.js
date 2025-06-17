e6237fdc9cbd6f85f273697be146d0275ddc41da
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-17 18:57:08 +0600

    json9

---
+++ JSON Fetch Promise/json9.js
@@ -0,0 +1,14 @@
+const course = {
+
+    cTitle: "Javasript",
+
+    instructorName: "Jonkar Mahmud",
+
+    duration: "60 hours",
+
+    lessons: [lessonName = "JSON",duration = "2 hours",difficultyLevel = "Easy"]
+}
+
+const courseJSON = JSON.stringify(course);
+
+console.log(courseJSON);
