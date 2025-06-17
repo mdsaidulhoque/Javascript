3804a4b5b7db83a88758ff04e2b11100bc080220
md-saidul-hoque <mdsaidulhoque951@gmail.com>
2025-06-17 18:56:40 +0600

    json7

---
+++ JSON Fetch Promise/json7.js
@@ -0,0 +1,14 @@
+const movie = {
+
+    title: "Inception",
+
+    releaseYear: 2011,
+
+    actor: "Leonardo",
+
+    rating: 10
+}
+
+const movieJSON = JSON.stringify(movie);
+
+console.log(movieJSON);
